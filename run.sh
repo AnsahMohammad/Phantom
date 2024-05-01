49ff65783b0d09f8dd3549a51fd006b3e73ee6c6
AnsahMohammad <mohdansah10@gmail.com>
2024-05-01 19:53:10 +0530

    feat: added run for render deploy

---
+++ run.sh
@@ -0,0 +1,12 @@
+python3 -m venv .env
+source .env/bin/activate
+
+pip install -r requirements.txt
+python3 -m src.phantom --num_threads 10 --urls "https://www.geeksforgeeks.org/" "https://stackoverflow.com/questions" "https://en.wikipedia.org/wiki/India" "https://developers.cloudflare.com/" "https://bloggingidol.com/best-programming-blogs/" --show_logs True --print_logs True --sleep 600
+
+clear
+echo "Installation done"
+python3 -m src.phantom_indexing
+echo "Phantom Processing done"
+clear
+python3 -m src.query_engine
