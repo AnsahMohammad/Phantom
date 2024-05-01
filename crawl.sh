3241cffe7d6b04ac84accd15c44afe5fc341a6ab
Ansah Mohammad <mohdansah10@gmail.com>
2024-05-01 19:17:44 +0530

    test: added workflow (#4)
    * feat: initial commit
    
    * chore: cleanup
    
    * fix: code fix
    
    * test: added tests
    
    * test: updated trigger
    
    * test: updated nltk lib
    
    * test: updated flask test
    
    * test: optimized testing

--- crawl.sh
+++ crawl.sh
@@ -2,4 +2,4 @@ python3 -m venv .env
 source .env/bin/activate
 
 pip install -r requirements.txt
-python3 -m src.phantom --num_threads 10 --urls "https://www.geeksforgeeks.org/" "https://stackoverflow.com/questions" --show_logs True --print_logs True --sleep 60
+python3 -m src.phantom --num_threads 10 --urls "https://www.geeksforgeeks.org/" "https://stackoverflow.com/questions" "https://en.wikipedia.org/wiki/India" "https://developers.cloudflare.com/" --show_logs True --print_logs True --sleep 600
