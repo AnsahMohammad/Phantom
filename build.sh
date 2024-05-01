39821563ab6049988606bd75a26eca3927967038
AnsahMohammad <mohdansah10@gmail.com>
2024-05-01 22:09:43 +0530

    chore: final deploy

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ python3 -m venv .env
 source .env/bin/activate
 
 pip install -r requirements.txt
-python3 -m src.phantom --num_threads 10 --urls "https://www.geeksforgeeks.org/" "https://stackoverflow.com/questions" "https://en.wikipedia.org/wiki/India" "https://developers.cloudflare.com/" "https://bloggingidol.com/best-programming-blogs/" --show_logs True --print_logs True --sleep 20
+python3 -m src.phantom --num_threads 10 --urls "https://www.geeksforgeeks.org/" "https://stackoverflow.com/questions" "https://en.wikipedia.org/wiki/India" "https://developers.cloudflare.com/" "https://bloggingidol.com/best-programming-blogs/" --show_logs True --print_logs True --sleep 600
 
 clear
 echo "Installation done"
