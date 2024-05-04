e2d3785f28c856fe75fa7b4ff5ecedc1009393ee
AnsahMohammad <mohdansah10@gmail.com>
2024-05-04 13:29:51 +0530

    fix: updated query_engine

--- local_search.sh
+++ local_search.sh
@@ -3,7 +3,5 @@ source .env/bin/activate
 pip install -r requirements.txt
 clear
 echo "Installation done"
-python3 -m src.phantom_indexing
-echo "Phantom Processing done"
 clear
 python3 -m src.query_engine
