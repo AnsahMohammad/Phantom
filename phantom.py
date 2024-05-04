a189ede22dde4275a3f708a6268385b5ea6cb1ac
AnsahMohammad <mohdansah10@gmail.com>
2024-05-04 14:00:44 +0530

    feat: transfered the query to GET method

--- phantom.py
+++ phantom.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from src.query_engine import Phantom_Query
 from src.phantom_engine import Parser
 
@@ -6,27 +6,22 @@ app = Flask(__name__)
 engine = Phantom_Query("indexed.json", titles="titles.json")
 parser = Parser()
 
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def home():
-    input_text = ""
-    if request.method == "POST":
-        input_text = request.form.get("input_text")
+    input_text = request.args.get('q', '')
+    if input_text:
         result = process_input(input_text)
         return render_template("result.html", result=result, input_text=input_text)
-    return render_template("home.html", input_text=input_text)
-
+    return render_template("home.html")
 
 def process_input(input_text):
     result = engine.query(input_text, count=20)
     # (doc, score, title)
     return result
 
-
 @app.route("/health", methods=["GET"])
 def health_check():
     return "OK", 200
-
 
 if __name__ == "__main__":
     app.run()
