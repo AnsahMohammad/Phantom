1ca1c7435e71032d12893039360578255f389dd1
AnsahMohammad <mohdansah10@gmail.com>
2024-05-08 06:18:56 +0530

    refactor: rename src to phantom

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ pip install -r requirements.txt
 if [ -z "$SUPABASE_URL" ] || [ -z "$SUPABASE_KEY" ]; then
     sleep 1
     echo "SUPABASE_URL and SUPABASE_KEY are not set. Crawling..."
-    python3 -m src.phantom --num_threads 5 --urls "https://www.geeksforgeeks.org/" "https://en.wikipedia.org/wiki/India" "https://developers.cloudflare.com/" "https://bloggingidol.com/best-programming-blogs/" "https://www.hindustantimes.com/india-news/" "https://www.bbc.com/news" --show_logs True --print_logs True --sleep 240
+    python3 -m phantom.phantom --num_threads 5 --urls "https://www.geeksforgeeks.org/" "https://en.wikipedia.org/wiki/India" "https://developers.cloudflare.com/" "https://bloggingidol.com/best-programming-blogs/" "https://www.hindustantimes.com/india-news/" "https://www.bbc.com/news" --show_logs True --print_logs True --sleep 240
 else
     echo "SUPABASE_URL and SUPABASE_KEY are set. Not crawling."
 fi
@@ -20,7 +20,7 @@ echo "Installation done"
 python3 -m nltk.downloader stopwords
 python3 -m nltk.downloader punkt
 ls
-python3 -m src.phantom_indexing
+python3 -m phantom.phantom_indexing
 echo "Phantom Processing done"
 clear
 echo "Build done"
