c6cc97b37c26a73ef8af19754bda486a4a409345
AnsahMohammad <mohdansah10@gmail.com>
2024-05-09 10:40:49 +0530

    fix: adde nltk dowbload in search

--- search.sh
+++ search.sh
@@ -4,13 +4,15 @@ python3 -m venv .env
 source .env/bin/activate
 
 pip install -r requirements.txt
-
+python3 -m nltk.downloader stopwords
+python3 -m nltk.downloader punkt
 
 export IDF_CONTENT=0
 export IDF_TITLE=1
 export CHUNK_SIZE=500
 export CHUNK_LIMIT=2000
 
+echo "running the server"
 
 # python3 phantom.py
 gunicorn -w 1 server:app
