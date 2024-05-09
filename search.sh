91d93240812a231c937838c1b1dfc209c6e0eb69
AnsahMohammad <mohdansah10@gmail.com>
2024-05-09 10:29:17 +0530

    feat: added vars in search script

--- search.sh
+++ search.sh
@@ -4,5 +4,13 @@ python3 -m venv .env
 source .env/bin/activate
 
 pip install -r requirements.txt
+
+
+export IDF_CONTENT=0
+export IDF_TITLE=1
+export CHUNK_SIZE=500
+export CHUNK_LIMIT=2000
+
+
 # python3 phantom.py
 gunicorn -w 1 server:app
