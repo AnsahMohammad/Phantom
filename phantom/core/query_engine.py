ced71c51c56a756408ce6f0522b4ae8eb278406f
Ansah Mohammad <mohdansah10@gmail.com>
2024-05-11 11:57:16 +0530

    fix: Deployment errors fixed (#20)
    * fix: deploy error for content
    
    * fix: bug fix
    
    * fix: bug fix2
    
    * fix: data

--- phantom/core/query_engine.py
+++ phantom/core/query_engine.py
@@ -32,6 +32,7 @@ class Phantom_Query:
         self.CONTENT_WEIGHT = 1
         self.TITLE_WEIGHT = 3
 
+        self.data = {}
         self.load(filename)
 
         if title_path or self.remote_db:
@@ -44,28 +45,27 @@ class Phantom_Query:
             else:
                 self.log("Titles loaded", "Query_Engine")
 
-        # self.tf = self.data["tf"]
-        self.idf = self.data["idf"]
-        self.tfidf = self.data["tfidf"]
-
-        self.t_idf = self.t_data["idf"]
-        self.t_tfidf = self.t_data["tfidf"]
-
-        self.lookup = set(self.idf.keys())
-        self.t_lookup = set(self.t_idf.keys())
         self.log("Query Engine Ready", "Query_Engine")
 
         self.stemmer = PorterStemmer()
         self.stop_words = set(stopwords.words("english"))
 
     def load(self, filename):
-        self.data = {}
-        with open(filename + ".json", "r") as f:
-            self.data = json.load(f)
+        if self.IDF_CONTENT:
+            with open(filename + ".json", "r") as f:
+                self.data = json.load(f)
+                # self.tf = self.data["tf"]
+                self.idf = self.data["idf"]
+                self.tfidf = self.data["tfidf"]
+            self.lookup = set(self.idf.keys())
 
-        self.t_data = {}
-        with open("title_" + filename + ".json", "r") as f:
-            self.t_data = json.load(f)
+        if self.IDF_TITLE:
+            self.t_data = {}
+            with open("title_" + filename + ".json", "r") as f:
+                self.t_data = json.load(f)
+            self.t_idf = self.t_data["idf"]
+            self.t_tfidf = self.t_data["tfidf"]
+            self.t_lookup = set(self.t_idf.keys())
 
     def query(self, query, count=10):
         self.log(f"Query received : {query}", "Query_Engine")
