55250aa6834b1d9633470e94ecd928de8d0ad935
AnsahMohammad <mohdansah10@gmail.com>
2024-05-11 10:56:33 +0530

    chore: updated worker to 2mins runtime

--- crawl.sh
+++ crawl.sh
@@ -5,4 +5,4 @@ python3 -m venv .env
 source .env/bin/activate
 
 pip install -r requirements.txt
-python3 -m phantom.main --num_threads 3 --urls "https://www.aljazeera.com/" "https://www.bbc.com/news" "https://www.ndtv.com/" --show_logs True --sleep 300
+python3 -m phantom.main --num_threads 3 --urls "https://www.aljazeera.com/" "https://www.bbc.com/news" "https://www.ndtv.com/" --show_logs True --sleep 120
