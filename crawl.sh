0556053a5b2d74b671d02bded5188b17a954c49b
Ansah Mohammad <mohdansah10@gmail.com>
2024-05-11 11:08:06 +0530

    chore: Bump 6 | Background worker and log errors
    * feat: added error logging to remote
    
    * fix: fixed responsivness
    
    * refactor: changed the content to h1h2h3 instead of text
    
    * feat: add remote background worker and scheduler
    
    * chore: added development plan in readme
    
    * fix: fixed logger error
    
    * fix: logger error
    
    * chore: updated secrets
    
    * chore: updated secrets
    
    * chore: updated worker to 2mins runtime
    
    * chore: updated url

--- crawl.sh
+++ crawl.sh
@@ -1,8 +1,9 @@
 # crawler script to crawl the given urls
 # background crawler to update the indexer for realtime data
+# background crawler to update the indexer for realtime data
 
 python3 -m venv .env
 source .env/bin/activate
 
 pip install -r requirements.txt
-python3 -m phantom.main --num_threads 3 --urls "https://www.aljazeera.com/" "https://www.bbc.com/news" "https://www.ndtv.com/" --show_logs True --sleep 300
+python3 -m phantom.main --num_threads 3 --urls "https://www.aljazeera.com/" "https://www.bbc.com/news" "https://www.ndtv.com/" --show_logs True --sleep 120
